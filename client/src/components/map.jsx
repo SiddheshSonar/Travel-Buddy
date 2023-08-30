e2e0a86dba47a2ae9b1310b9b84afab05be4621e
stephenvaz <stephenvaz2003@gmail.com>
2023-08-30 22:24:45 +0530

    added map support

--- client/src/components/map.jsx
+++ client/src/components/map.jsx
@@ -1,34 +1,82 @@
 "use client"
 
+import 'mapbox-gl/dist/mapbox-gl.css';
 import React from "react";
-import DeckGL from "@deck.gl/react";
-import ReactMapGL, { StaticMap } from 'react-map-gl';
+import { useRef } from 'react';
+// import DeckGL from "@deck.gl/react";
+import ReactMapGL, { Marker, MapRef } from 'react-map-gl';
+import { BiCurrentLocation } from "react-icons/bi"
+import "./cust.css"
 
-// Set your mapbox access token here
-const MAPBOX_ACCESS_TOKEN ="pk.eyJ1IjoiZ25zbXRlc3QiLCJhIjoiY2xpdHlxdDVhMHRicjNubzdweDEyZmYweiJ9.Ry25g8XRnQ7ZaatTwsiBRw";
-
-// Viewport settings
-const INITIAL_VIEW_STATE = {
-  longitude: -122.41669,
-  latitude: 37.7853,
-  zoom: 13,
-  pitch: 0,
-  bearing: 0
-};
-
+const MAPBOX_ACCESS_TOKEN = "pk.eyJ1IjoiZ25zbXRlc3QiLCJhIjoiY2xpdHlxdDVhMHRicjNubzdweDEyZmYweiJ9.Ry25g8XRnQ7ZaatTwsiBRw";
 const layers = [];
 
-const Map = () => {
+const MyMap = () => {
+  const mapRef = useRef();
+
+  // get location 
+  const [location, setLocation] = React.useState({});
+  React.useEffect(() => {
+    navigator.geolocation.getCurrentPosition(function (position) {
+      setLocation(position.coords);
+      setViewport({
+        latitude: position.coords.latitude,
+        longitude: position.coords.longitude,
+        zoom: 11
+      });
+    });
+
+
+  }, []);
+
+  const [viewport, setViewport] = React.useState({
+    latitude: 38.958630,
+    longitude: -77.357002,
+    zoom: 8
+  });
   return (
-    <DeckGL
-      initialViewState={INITIAL_VIEW_STATE}
-      controller={true}
-      layers={layers}
-    >
-      <StaticMap mapboxApiAccessToken={MAPBOX_ACCESS_TOKEN} />
-    </DeckGL>
+    <div style={{
+      width: "100%",
+      height: "100vh",
+    }}>
+      <ReactMapGL
+        ref={mapRef}
+        {...viewport}
+        interactive={true}
+
+        mapStyle="mapbox://styles/gnsmtest/cllxwulk000kv01peb9yk15a9"
+        onMove={evt => setViewport(evt.viewState)}
+
+        mapboxAccessToken={"pk.eyJ1IjoiZ25zbXRlc3QiLCJhIjoiY2xseHc5d3plMmt0eDNlcGU4NmN2eXk4aCJ9.qjuYscqJ5dSJMS3XJYmmxQ"}
+      >
+        <div
+          className='rounded-full  w-12 h-12 flex items-center justify-center'
+          style={{ position: 'absolute', left: 10, top: 10, backgroundColor: '#00000099' }}
+          onClick={() => {
+            navigator.geolocation.getCurrentPosition(function (position) {
+              mapRef.current?.flyTo({ center: [position.coords.longitude, position.coords.latitude], duration: 2000 });
+            });
+          }}
+        >
+          <BiCurrentLocation
+            title='Current Location'
+            size={30}
+            color="lightblue"
+          />
+        </div>
+
+
+
+        <Marker
+          latitude={location.latitude}
+          longitude={location.longitude}
+          offsetLeft={-20}
+          offsetTop={-10}
+        />
+      </ReactMapGL>
+    </div>
   );
 };
 
 
-export default Map;
+export default MyMap;

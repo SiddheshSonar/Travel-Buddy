2ba6c95b67e9928241cf8155f65652c1d564114e
Stephen Vaz <stephenvaz2003@gmail.com>
2023-08-30 18:22:30 +0530

    image fix

--- client/src/app/auth/components/navB.jsx
+++ client/src/app/auth/components/navB.jsx
@@ -4,21 +4,26 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import Logo from '../assets/TB-logo.png'
+import Image from 'next/image'
+
 const NavB = () => {
 
     return (
         <Navbar collapseOnSelect expand="lg" bg='dark' variant='dark'>
             <Container>
-            <Navbar.Brand href="#home">
-            <img
+                <Navbar.Brand href="#home">
+                    {/* <img
               alt=""
               src={Logo}
               width="30"
               height="30"
               className="d-inline-block align-top"
-            />{' '}
-            Travel Buddy
-          </Navbar.Brand>
+            />{' '} */}
+                    <Image src={Logo} roundedCircle width="50"
+                        height="50" className="d-inline-block align-top"
+                    />
+                    Travel Buddy
+                </Navbar.Brand>
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav className="ms-auto">

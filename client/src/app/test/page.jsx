1faf1f34116e11576a603be3ceb6dda1e52e50df
stephenvaz <stephenvaz2003@gmail.com>
2023-08-30 22:24:45 +0530

    added map support

--- client/src/app/test/page.jsx
+++ client/src/app/test/page.jsx
@@ -1,12 +1,12 @@
 "use client"
-import Map from '@/components/map';
+import MyMap from '@/components/map';
 import React from 'react';
 
 const Page = () => {
     return (
         <div>
             {/* <h1>Map Component</h1> */}
-            <Map/>
+            <MyMap/>
         </div>
     );
 }
